698aea0b58df731795fcad308ee24d66d650b6b7
KANGDONGHWI <rkdehdgnl@naver.com>
2021-08-24 15:15:35 +0900

    Chore: 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import Tokens from 'utils/Tokens';
 
 function App() {
-  return <div className="App">App</div>;
+  const task = {
+    id: 1,
+    taskName: '자소서 쓰기',
+    status: 'status.ONGOING',
+    createdAt: '2021-02-03',
+    updatedAt: '2021-07-07',
+  };
+  const tokens = Tokens.getInstance();
+  tokens.save(task);
+  const accessToken = tokens.load();
+  console.log(accessToken);
+  const HandleClear = () => {
+    tokens.clear();
+  };
+
+  return (
+    <>
+      <div className="App">{accessToken.taskName}</div>
+      <button onClick={HandleClear}>지우기</button>
+    </>
+  );
 }
 
 export default App;
