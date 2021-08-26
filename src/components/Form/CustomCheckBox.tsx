cde2942b6e36b6fa0812682c34d93ae53a00465c
hami-dev <rinrin.xml@gmail.com>
2021-08-26 20:11:32 +0900

    Refactor: CustomCheckBox 코드 수정 및 디자인 변경

--- src/components/Form/CustomCheckBox.tsx
+++ src/components/Form/CustomCheckBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { ReactElement } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
@@ -11,31 +11,29 @@ interface CustomCheckBoxProps {
   checkHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-type Props = {
-  checked?: boolean;
-};
 const CustomCheckBox: React.FC<CustomCheckBoxProps> = ({
   className,
   labelText,
   id,
   checked,
   checkHandler,
-}) => {
+}): ReactElement => {
   return (
     <Wrapper className={className}>
-      <StyledCheckBox checked={checked}>
-        <CheckIcon>
-          <FontAwesomeIcon icon={faCheck} />
-        </CheckIcon>
-      </StyledCheckBox>
-
-      <HiddencheckBox
-        type="checkbox"
-        id={id}
-        checked={checked}
-        onChange={checkHandler}
-      />
-      <label htmlFor={id}>{labelText}</label>
+      <Label checked={checked}>
+        <StyledCheckBox checked={checked}>
+          <CheckIcon>
+            <FontAwesomeIcon icon={faCheck} />
+          </CheckIcon>
+        </StyledCheckBox>
+        <HiddencheckBox
+          type="checkbox"
+          id={id}
+          checked={checked}
+          onChange={checkHandler}
+        />
+        {labelText}
+      </Label>
     </Wrapper>
   );
 };
@@ -43,11 +41,17 @@ const CustomCheckBox: React.FC<CustomCheckBoxProps> = ({
 export default CustomCheckBox;
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
   font-size: 1.3rem;
   margin-top: 1rem;
   margin-right: 2rem;
+`;
+
+const Label = styled.label<{ checked: boolean }>`
+  display: flex;
+  align-items: center;
+  color: ${({ checked }) => (checked ? 'black' : '#ccccd5')};
+  user-select: none;
+  cursor: pointer;
 `;
 
 const HiddencheckBox = styled.input`
@@ -61,22 +65,17 @@ const HiddencheckBox = styled.input`
   margin: -1px;
   padding: 0;
 `;
+
 const CheckIcon = styled.div`
   font-size: 1.6rem;
-  color: black;
 `;
 
-const StyledCheckBox = styled.div<Props>`
+const StyledCheckBox = styled.div<{ checked: boolean }>`
   width: 1.8rem;
   height: 1.8rem;
-  border: ${Props =>
-    Props.checked ? '1px solid transparent' : '1px solid #ccccd5'};
-  border-radius: 0.5rem;
-  background-color: ${Props => (Props.checked ? 'transparent' : '#fff')};
   margin-right: 0.5rem;
 
   ${CheckIcon} {
-    visibility: ${Props => (Props.checked ? 'visible' : 'hidden')};
-    color: #47a547;
+    color: ${({ checked }) => (checked ? 'black' : '#ccccd5')};
   }
 `;
